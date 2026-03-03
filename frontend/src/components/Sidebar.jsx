0c8bd6b5182c68ab4262cdb67aecb34437899f72
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2026-03-02 22:11:18 -0500

    style: add sidebar styling with full viewport height and border

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom'
+import './Sidebar.css';
 
 export default function Sidebar() {
   return (
-    <nav>
+    <nav className='nav-sidebar'>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/exercises">Exercises</Link></li>
